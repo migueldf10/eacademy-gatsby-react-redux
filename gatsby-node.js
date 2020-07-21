4bde099057e54e374eeda9152b04e599f34acf85
migueldf10 <migueldf10@gmail.com>
2020-07-21 13:01:59 +0200

    cleaned console

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,11 @@
 const axios = require('axios');
-
 const get = endpoint => axios.get(`http://localhost:4000${endpoint}`);
-
 
 exports.createPages = async ({ actions: { createPage } }) => {
 	try {
 		const response = await get('/course')
 		const courses = response.data.rows
 		courses.map(course => {
-			console.log(course)
 
 			createPage({
 				path: `/courses/${course.id}`,
