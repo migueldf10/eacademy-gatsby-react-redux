8d48509c5255bfb1d5ab7445b2aa81929b621b63
migueldf10 <migueldf10@gmail.com>
2020-07-30 14:13:39 +0200

    Lesson action from server

--- src/state/lesson/actions.ts
+++ src/state/lesson/actions.ts
@@ -3,19 +3,12 @@ export const SET_ACTIVE_LESSON = "SET_ACTIVE_LESSON"
 
 export const setActiveLesson = lessonId => {
   return async (dispatch, getState) => {
-    if (!getState().course.lessons) return null
-    const currentLesson = getState().course.lessons.find(lesson => {
-      // console.log("finding lesson with id", lessonId)
-      // console.log("now checking lesson with id", lesson.id)
-      return parseInt(lesson.id) === parseInt(lessonId)
-    })
-    // console.log("current lesson", currentLesson)
-    // const response = await buildAxios(getState().session.tokens.idToken).get(
-    //   `/myCourses/${courseId}`
-    // )
+    const response = await buildAxios(getState().session.tokens.idToken).get(
+      `/lessons/${lessonId}`
+    )
     dispatch({
       type: SET_ACTIVE_LESSON,
-      payload: currentLesson,
+      payload: response.data,
     })
   }
 }
