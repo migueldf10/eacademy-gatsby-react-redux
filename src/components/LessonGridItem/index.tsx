2ee9e406a17c64b1769283d8938e700e60303c69
migueldf10 <migueldf10@gmail.com>
2020-07-30 15:48:42 +0200

    Lesson grid item now displays if completed or not

--- src/components/LessonGridItem/index.tsx
+++ src/components/LessonGridItem/index.tsx
@@ -1,6 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
+import { LessonGridItemContainer } from "./styled"
 export default function LessonGridItem(props) {
   const { lesson } = props
-  return <Link to={`/lessons/${lesson.id}`}>{lesson.title}</Link>
+  console.log(lesson)
+  return (
+    <LessonGridItemContainer as={Link} to={`/lessons/${lesson.id}`}>
+      {lesson.title}
+      {lesson.toDo && (
+        <div>
+          <span>Marked as review later</span>
+        </div>
+      )}
+      {lesson.completed && (
+        <div>
+          <span>FINISHED</span>
+        </div>
+      )}
+    </LessonGridItemContainer>
+  )
 }
