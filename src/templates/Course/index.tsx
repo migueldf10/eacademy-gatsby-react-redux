a47fda17fb7cac1aeafcfb1755972aa064bc46ea
migueldf10 <migueldf10@gmail.com>
2020-08-02 22:32:15 +0200

    button

--- src/templates/Course/index.tsx
+++ src/templates/Course/index.tsx
@@ -3,18 +3,24 @@ import Layout from "../../components/Layout"
 import Styled from "./styled"
 import { Button, VideoEmbed } from "../../components/Ui"
 import { addToCart } from "../../state/commerce/actions"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { Hero } from "../../components/Ui"
 import locales from "../../utils/locales"
+import { getCourses } from "../../state/session/selectors"
+import { Link } from "@reach/router"
 
 export default function coursePage(props) {
   const { course } = props.pageContext
   const dispatch = useDispatch()
+  const courses = useSelector(getCourses)
 
   const addToCartAction = () => {
     dispatch(addToCart(course))
   }
 
+  const courseIds = courses.map(course => course.id)
+  const alreadyBought = courseIds.includes(course.id)
+  console.log(alreadyBought)
   return (
     <Layout>
       <Hero.WrapperWithOverlap>
@@ -28,11 +34,21 @@ export default function coursePage(props) {
           <VideoEmbed.Youtube url={course.videoUrl} />
         </Styled.IntroFrame>
         <p>{course.price} Euros</p>
-        <p>State: {course.published ? "Published" : "Draft"}</p>
+        {/* <p>State: {course.published ? "Published" : "Draft"}</p> */}
         <p>{course.description}</p>
-        <Button.ContrastPrimary onClick={addToCartAction}>
-          {locales("cart.addToCart")}
-        </Button.ContrastPrimary>
+        {!alreadyBought ? (
+          <Button.ContrastPrimary onClick={addToCartAction}>
+            {locales("cart.addToCart")}
+          </Button.ContrastPrimary>
+        ) : (
+          <>
+            <span>You already own this course!</span>
+            <br />
+            <Button.ContrastPrimary as={Link} to={`/my-courses/${course.id}`}>
+              View the course page
+            </Button.ContrastPrimary>
+          </>
+        )}
       </Styled.Container>
     </Layout>
   )
