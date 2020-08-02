940c6dbdc4c4884c2939d4d45d6b346603259659
migueldf10 <migueldf10@gmail.com>
2020-08-02 15:46:30 +0200

    Grid responsivenes

--- src/theme/mixins/grids.tsx
+++ src/theme/mixins/grids.tsx
@@ -13,10 +13,10 @@ const grid = css`
   }
   @media (max-width: 568px) {
     grid-template-columns:
-      [LB] 1fr [LXL] 1fr [LS] repeat(2, [C] 22ch)
+      [LB] 1fr [LXL] 1fr [LS] repeat(2, [C] 20ch)
       [RS] 1fr [RXL] 1fr [RB];
   }
-  @media (max-width: 370px) {
+  @media (max-width: 420px) {
     grid-template-columns:
       [LB] 1fr [LXL] 1fr [LS] repeat(2, [C] 15ch)
       [RS] 1fr [RXL] 1fr [RB];
