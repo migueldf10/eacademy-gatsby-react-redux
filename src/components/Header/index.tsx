c39ca70630094bdd032891aeeb6a6e025513f8c1
migueldf10 <migueldf10@gmail.com>
2020-08-03 09:00:41 +0200

    Header lets you recover that cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,17 @@
-import { Link, useStaticQuery, graphql } from "gatsby"
 import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import { useSelector, useDispatch } from "react-redux"
 import Styled from "./styled"
 import { login } from "../../utils/auth"
 import { Button } from "../Ui"
 import { getUser } from "../../state/session/selectors"
-import { useSelector } from "react-redux"
 import getLocales from "../../utils/locales"
-const Header = props => {
-  const user = useSelector(getUser)
+import { getUiState } from "../../state/ui/selectors"
+import { showCart } from "../../state/ui/actions"
+import { getCart } from "../../state/commerce/selectors"
 
+const Header = () => {
+  const user = useSelector(getUser)
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -19,6 +22,9 @@ const Header = props => {
     }
   `)
   const { title } = data.site.siteMetadata
+  const uiState = useSelector(getUiState)
+  const cart = useSelector(getCart)
+  const dispatch = useDispatch()
 
   return (
     <Styled.Wrapper>
@@ -27,6 +33,9 @@ const Header = props => {
           <Link to="/">{title}</Link>
         </h1>
         <Link to="/">Shop</Link>
+        {!uiState.displayCart && cart[0] && cart[0].id && (
+          <a onClick={() => dispatch(showCart())}>Cart</a>
+        )}
         <Styled.Navigation>
           {user.nickname ? (
             <Link to="/account">{getLocales("ui.myAccount")}</Link>
